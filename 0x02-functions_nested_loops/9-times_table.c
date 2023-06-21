4970ec8e12fa35b24a8ac86468efa95ed2d67912
ramy hesham <ramyhesham372@gmail.com>
2023-06-21 11:57:21 -0700

    yask 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * times_table - Entry point
+ *
+ * Description: print time table from 0 to 9
+ *
+ * Return: void
+ *
+ */
+
+void times_table(void)
+{
+	int i, n, mult;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(48);
+		for (n = 0; n <= 9; n++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			mult = i * n;
+
+			if (mult <= 9)
+				_putchar(' ');
+			else
+				_putchar((mult / 10) + 48);
+
+			_putchar((mult % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}

f625cc91bc8889e85f59bcf52424ca6bdd03ed3b
RamyHesham372 <ramyhesham372@gmail.com>
2023-08-01 05:19:01 -0700

    second task in single linked list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * listint_len - travers all the nodes.
+ *
+ * @h: the pointer of the first node.
+ *
+ * Return: the number of the elements.
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	int count = 0;
+	const listint_t *temp = h;
+
+	while (temp != NULL)
+	{
+		count++;
+		temp = temp->next;
+	}
+	return count;
+}
+

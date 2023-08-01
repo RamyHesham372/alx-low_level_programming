73b751d09fdfe57d562aa17a2a968cec2a4704a1
RamyHesham372 <ramyhesham372@gmail.com>
2023-08-01 13:55:26 -0700

    edit task 7,9 singly linked list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -15,13 +15,13 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	temp = head;
 
-	if (temp == NULL)
-		return (NULL);
-
-	for (i = 0; i < index; i++)
+	for (i = 0; temp != NULL && i < index; i++)
 	{
 		temp = temp->next;
 	}
+
+	if (!temp)
+		return (NULL);
 	return (temp);
 }
 
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (temp);
 	}
 
-	for (i = 0; i < idx - 1; i++)
+	for (i = 0; ptr != NULL && i < idx - 1; i++)
 	{
 		ptr = ptr->next;
 	}

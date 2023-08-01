d19f9594d117dfc3ea6b3f46162da7887121fdf6
RamyHesham372 <ramyhesham372@gmail.com>
2023-08-01 05:57:54 -0700

    second singly linked list

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - adds new nodes at the beginning.
+ *
+ * @head: the pointer of the first node.
+ * @n: the value of the node.
+ *
+ * Return: the new address of the first node.
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *temp = malloc(sizeof(listint_t));
+
+	temp->n = n;
+	temp->next = NULL;
+
+	temp->next = *head;
+	*head = temp;
+	return (*head);
+}
+
